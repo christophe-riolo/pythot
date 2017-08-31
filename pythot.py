a1184b96b6ed45958079d22c826954454a679b03
Rosslaew <rosslaew@gmail.com>
2017-08-31 12:14:48 +0300

    Added code to display the window.
    The window could only be seen in Qt designer, now we can launch
    pythot.py to see it, though it doesn't do anything yet.

---
+++ pythot.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import sys
+from PyQt5.QtWidgets import QApplication
+from PyQt5 import uic
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ui = uic.loadUi("pythot.ui")
+    main_window = ui.window()
+    main_window.show()
+    sys.exit(app.exec_())
