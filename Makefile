be97956643b8b94352e8e38c886118958f76d0f3
Rosslaew <rosslaew@gmail.com>
2017-09-02 20:13:13 +0300

    More modularity added.
    Reorganised the files to make them into a nice pythot package.
    I had to modify the way the ui was used, to make python files inside the
    module instead of loading the .ui file directly.

--- Makefile
+++ Makefile
@@ -1,5 +1,15 @@
 RESOURCE_FILE = resources.qrc
-RESOURCE = resources.py
+RESOURCE = pythot/resources_rc.py
+UI_FILE = pythot.ui
+UI = pythot/window.py
+
+all: ui resources
+
+ui: $(UI_FILE)
+	pyuic5 --from-imports -o $(UI) $(UI_FILE)
 
 resources: $(RESOURCE_FILE)
-	pyrcc5 -o $(RESOURCE) $(RESOURCE_FILE)
+	pyrcc5  -o $(RESOURCE) $(RESOURCE_FILE)
+
+clean:
+	rm -f $(RESOURCE) $(UI)
--- pythot/pythot.py
+++ pythot/pythot.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python3
 import sys
 
-from PyQt5 import uic
 from PyQt5.QtGui import QFontDatabase
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
-import resources
-import equations
+from .window import Ui_MainWindow
+from . import equations
+
+
+class Pythot(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    ui = uic.loadUi("pythot.ui")
+    main_window = Pythot()
 
     QFontDatabase.addApplicationFont(":/fonts/lmroman10-bolditalic.otf")
     QFontDatabase.addApplicationFont(":/fonts/lmroman10-regular.otf")
 
-    main_window = ui.window()
     main_window.show()
     sys.exit(app.exec_())
 
