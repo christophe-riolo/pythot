269238164539818642b33d218ad5124e89b7b1d4
Rosslaew <rosslaew@gmail.com>
2017-09-06 22:48:09 +0300

    Made OperationPrompt display.

--- pythot/equations.py
+++ pythot/equations.py
@@ -1,4 +1,6 @@
-from PyQt5.QtCore import QStringListModel
+from PyQt5.QtCore import QStringListModel, Qt
+
+from operator import add, sub, neg, inv, mul, truediv
 
 from sympy import S
 from sympy.abc import x
@@ -22,7 +24,7 @@ class Equation:
 
     def __str__(self):
         return (
-            " = ".join(self.left, self.right)
+            " = ".join(str(self.left), str(self.right))
             + '<img src=":/icons/thot.ico" />' if self.isSolved() else ""
             )
 
@@ -95,6 +97,17 @@ class Equation:
         return res
 
     def isSolved(self):
+        """Tells whether the equation is solved or not.
+
+        >>> Equation("x=2").isSolved()
+        True
+        >>> Equation("2=x").isSolved()
+        True
+        >>> Equation("0=2").isSolved()
+        True
+        >>> Equation("x=2*x").isSolved()
+        False
+        """
         # The l0 and r0 are the constant terms
         # and l1 and r1 are the x-dependent ones.
         l0, l1 = self.left.as_independent(x, as_Add=True)
@@ -109,27 +122,68 @@ class Operation:
     an equation.
     """
     def __call__(self, equation):
-        super().__init__()
-        self.operator(equation, self.operand)
+        """Applies the operation to an equation."""
 
-    def __init__(self, operator, operand):
+        if self.operand is None:
+            return self.operator(equation)
+        else:
+            return self.operator(equation, self.operand)
+
+    def __init__(self, operator, operand=None):
+        """Creates an operation wating to be applied to an equation.
+
+        The operator has to be a function from module operator.
+        Accepted values are add, sub, mul, truediv, neg and inv.
+        An operand must be given for add, sub, mul and truediv,
+        and no operand will be accepted for neg and inv. If this
+        is not respected, an AttributeError will be raised.
+        """
+
+        # Testing the arguments.
+        if operator in [inv, neg] and operand is not None:
+            raise AttributeError("No operand allowed with " + str(operator))
+        if operator in [add, sub, mul, truediv]:
+            if operand is None:
+                raise AttributeError("Operand is compulsory with " + str(operator))
+            elif not isinstance(operand, Equation) and not isinstance(operand, Expr):
+                raise AttributeError("Operand must be an Equation or a sympy Expr")
+        if operator not in [add, sub, mul, truediv, inv, neg]:
+            raise AttributeError("Unknown operator " + str(operator))
+
         self.operator = operator
         self.operand = operand
+
+    def __str__(self):
+        if self.operator == add:
+            return '+ ' + str(self.operand)
+        if self.operator == sub:
+            return '- ' + str(self.operand)
+        if self.operator == mul:
+            return '* ' + str(self.operand)
+        if self.operator == mul:
+            return '/ ' + str(self.operand)
+        if self.operator == inv:
+            return 'Inversion des membres.'
+        if self.operator == neg:
+            return 'Négation des membres.'
 
 
 class Equations(QStringListModel):
     """List of all steps made so far to solve the equation.
     """
-    def __init__(self, base_equation, *args, **kwargs):
-        super().__init__(self, *args, **kwargs)
-        self.data = [base_equation]
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.data = []
 
     # Used as a slot
-    def update(self, operation, operand):
+    def update(self, operation):
         rc = self.rowCount()
-        last_step = self.data(self.index(rc - 1, 0), 0)
-        result = operation(last_step, operand)  # TODO
-        if self.insertRow(rc):
-            self.setData(self.index(rc, 0), result)
+        last_step = self.data(
+            self.index(rc - 1, Qt.DisplayRole),
+            Qt.DisplayRole)
+        result = operation(last_step)
+        if self.insertRows(rc, 2):
+            self.setData(self.index(rc, Qt.DisplayRole), operation)
+            self.setData(self.index(rc + 1, Qt.DisplayRole), result)
 
 # vim: fdm=indent
--- pythot/pythot.py
+++ pythot/pythot.py
@@ -1,30 +1,132 @@
+"""Defines the classes used to specialize the basic
+Qt classes.
+
+Pythot : main window
+OperationPrompt: modal window used to ask the operation being made.
+"""
+
+from operator import add, sub, mul, truediv, inv, neg
+from fractions import Fraction as F
+from sympy import S
+
+from PyQt5 import QtCore
 from PyQt5.QtWidgets import QMainWindow, QDialog
 from PyQt5.QtCore import pyqtSignal
 
 from .window import Ui_MainWindow
 from .operation import Ui_operation
-from . import equations
+from .equations import Operation, Equations
 
 
 class Pythot(QMainWindow, Ui_MainWindow):
-    def __init__(self):
-        super().__init__()
+    """Main application window. Specializes QMainWindow.
+    """
+    operation_actions = {
+            "actionAjouter_un_nombre": {"operator": add},
+            "actionSoustraire_un_nombre": {"operator": sub},
+            "actionAjouter_un_terme_en_x": {"operator": add, "x": True},
+            "actionSoustraire_un_terme_en_x": {"operator": sub, "x": True},
+            "actionMultiplier_par_un_nombre": {"operator": mul},
+            "actionDiviser_par_un_nombre": {"operator": truediv},
+            "actionIntervertir_les_deux_membres": {"operator": inv},
+            "actionPrendre_l_oppos": {"operator": neg},
+    }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.mode = "decimal"
+        self.equations = Equations(*args, **kwargs)
+
         self.setupUi(self)
+
+    def onActionNeg(self):
+        """Instanctiates an Operation on a request of negating the equation.
+        """
+        self.equations.update(Operation(neg))
+
+    def onActionInv(self):
+        """Instanctiates an Operation on a request of inverting the equation.
+        """
+        self.equations.update(Operation(inv))
+
+    def operationPrompt(self):
+        """Starts the prompt to get the current operation."""
+        sender = self.sender().objectName()
+        prompt = OperationPrompt(self, **Pythot.operation_actions[sender])
+        prompt.exec()
 
 
 class OperationPrompt(QDialog, Ui_operation):
     """Stores the operation asked in this dialog, to send it as a signal."""
 
-    make_operation = pyqtSignal(equations.Operation)
+    make_operation = pyqtSignal(Operation)
 
-    def __init__(self, operation):
-        super().__init__()
-        self.operation = operation
+    def __init__(self, parent, operator, x=False):
+        super().__init__(parent)
+
+        self.operator = operator
+
         self.setupUi(self)
+
+        # I am not going to allow any other mode.
+        mode = self.parent().mode
+        if mode == "fraction":
+            self.toFraction()
+        else:
+            self.toDecimal()
+
+        if x:
+            self.x.show()
+        else:
+            self.x.hide()
+
+
+    def retranslateUi(self, operation):
+        """Sets the text left of the input accordingly to the
+        operation we are doing.
+        """
+        # We specialize only the text, so let's invoke super()
+        super().retranslateUi(self)
+
+        # we might want to translate later
+        _translate = QtCore.QCoreApplication.translate
+        if self.operator is add:
+            self.text.setText(_translate("operation", "Ajouter aux deux membres de l\'équation :"))
+        if self.operator is sub:
+            self.text.setText(_translate("operation", "Soustraire aux deux membres de l\'équation :"))
+        if self.operator is mul:
+            self.text.setText(_translate("operation", "Multiplier les deux membres de l\'équation par :"))
+        if self.operator is truediv:
+            self.text.setText(_translate("operation", "Diviser les deux membres de l\'équation par :"))
+        # OperationPrompt should not be created for neg and inv
 
     def accept(self):
         """Sending signal make_operation before accepting."""
-        self.make_operation.emit(self.operation)
+        numerator = self.numerator.text()
+        denominator = self.denominator.text()
+
+        numerator = numerator if numerator else "0"
+        denominator = (
+                denominator
+                if denominator and self.mode == "fraction"
+                else "1"
+                )
+
+        self.make_operation.emit(
+            Operation(
+                self.operator,
+                S(F(F(numerator), F(denominator)))
+            )
+        )
         super().accept()
+
+    def toDecimal(self):
+        self.fraction_line.hide()
+        self.denominator.hide()
+
+    def toFraction(self):
+        self.fraction_line.show()
+        self.denominator.show()
 
 # vim: fdm=indent
